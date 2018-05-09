ff2d3b4b595d6688238dedb95c389525b425ab65
Henry Rizzi <henry.rizzi@adops.com>
2018-05-09 14:53:10 -0500

    one class per contrib

--- tests.py
+++ tests.py
@@ -39,6 +39,30 @@ TEST_SQL = """select cast(1.25 as float) as foo  -- float check
                    , 'ƺƺƺƺ';
            """
 
+#gross
+TEST_SUM_SQL = """select 0 as grouping,
+                        'hello' as label,
+                        100 as part
+                union
+                select 0 as grouping,
+                        'hello' as label,
+                        50 as part
+                union
+                select 0 as grouping,
+                        'word' as label,
+                        20 as part
+                union 
+                select 1 as grouping,
+                    'hello' as label,
+                    150 as part
+                union
+                select 1 as grouping,
+                    'word' as label,
+                    20 as part
+                union
+                select 3 as grouping,
+                    NULL as label,
+                    170 as part"""
 
 def setUpModule():
     global sql_file
@@ -65,7 +89,7 @@ class TestResults(unittest.TestCase):
         formatted_results = formatter.formatted_results()
         self.assertEqual(formatted_results, EXPECTED)
 
-class TestContribFormatters(unittest.TestCase):
+class TestContribAffixFormatter(unittest.TestCase):
     AFFIX_EXPECTED = { 'error': False,
                        'response': [{
                                     'foo': 1.25,
@@ -80,32 +104,6 @@ class TestContribFormatters(unittest.TestCase):
                                 }],
                         'metadata': {'executed': 'N/A',  
                                      'params': {}}
-                      }
-    
-    COMPACT_EXPECTED = {
-                        'columns': ['foo', 'bar', 'quux', 'quuux'],
-                        'data': [
-                             (1.25, '2017-09-09', 'Hello', 'Björk Guðmundsdóttir'),
-                             (42, '2031-05-25', 'yo', 'ƺƺƺƺ')]
-    }
-    CSV_EXPECTED = 'foo,bar,quux,quuux\r\n1.25,2017-09-09,Hello,Björk Guðmundsdóttir\r\n42,2031-05-25,yo,ƺƺƺƺ\r\n'
-
-    SUM_EXPECTED = { 'error': False,
-                     'response': {'summary': {},
-                                  'result':[{
-                                             'foo': 1.25,
-                                             'bar': '2017-09-09',
-                                             'quux': 'Hello',
-                                             'quuux': 'Björk Guðmundsdóttir'
-                                         }, {
-                                             'foo': 42,
-                                             'bar': '2031-05-25',
-                                             'quux': 'yo',
-                                             'quuux': 'ƺƺƺƺ'
-                                         }]
-                                },
-                     'metadata': {'executed': 'N/A',  
-                                  'params': {}}
                       }
 
     def test_results_affix(self):
@@ -122,27 +120,105 @@ class TestContribFormatters(unittest.TestCase):
         self.assertEqual(formatted_results['metadata']['params'],
                          self.AFFIX_EXPECTED['metadata']['params'])
 
+
+class TestContribCompactFormatter(unittest.TestCase):
+    COMPACT_EXPECTED = {
+                        'columns': ['foo', 'bar', 'quux', 'quuux'],
+                        'data': [
+                             (1.25, '2017-09-09', 'Hello', 'Björk Guðmundsdóttir'),
+                             (42, '2031-05-25', 'yo', 'ƺƺƺƺ')]
+    }
+
     def test_results_compact(self):
         loader = Query(query_name)
         result = loader.result_set()
         formatter = ResultFormat(result, format_='compact')
         formatted_results = formatter.formatted_results()
         self.assertEqual(formatted_results, self.COMPACT_EXPECTED)
-    
+
+class TestContribCSVFormatter(unittest.TestCase):
+    CSV_EXPECTED = 'foo,bar,quux,quuux\r\n1.25,2017-09-09,Hello,Björk Guðmundsdóttir\r\n42,2031-05-25,yo,ƺƺƺƺ\r\n'
+
     def test_results_csv(self):
         loader = Query(query_name)
         result = loader.result_set()
         formatter = ResultFormat(result, format_='csv')
         formatted_results = formatter.formatted_results()
         self.assertEqual(formatted_results, self.CSV_EXPECTED) 
+
+class TestContribSumFormatter(unittest.TestCase):
+    SUM_EXPECTED = {'error': False,
+                    'response': {'summary': {"hello": {
+                                                    "label": "hello",
+                                                    "part": 150
+                                                },
+                                                "overall_total": {
+                                                    "part": 170
+                                                },
+                                                "word": {
+                                                    "label": "word",
+                                                    "part": 20
+                                                }},
+                                 'result':[{
+                                        "label": "hello",
+                                        "part": 50
+                                    }, {
+                                        "label": "hello",
+                                        "part": 100
+                                    }, {
+                                        "label": "word",
+                                        "part": 20
+                                    }]
+                                },
+                     'metadata': {'executed': 'N/A',  
+                                  'params': {}}
+                      }
+    NO_SUM_EXPECTED = { 'error': False,
+                        'response': {'summary': {},
+                                     'result':[{
+                                                'foo': 1.25,
+                                                'bar': '2017-09-09',
+                                                'quux': 'Hello',
+                                                'quuux': 'Björk Guðmundsdóttir'
+                                            }, {
+                                                'foo': 42,
+                                                'bar': '2031-05-25',
+                                                'quux': 'yo',
+                                                'quuux': 'ƺƺƺƺ'
+                                            }]
+                                    },
+                        'metadata': {'executed': 'N/A',  
+                                    'params': {}}
+                      }
     
-    def test_results_sum(self):
+    def test_results_no_sum(self):
         loader = Query(query_name)
         result = loader.result_set()
         formatter = ResultFormat(result, format_='sum')
         formatted_results = formatter.formatted_results()
         self.assertEqual(formatted_results['response']['summary'],
-                         self.SUM_EXPECTED['response']['summary'])  
+                         self.NO_SUM_EXPECTED['response']['summary'])
+        self.assertEqual(formatted_results['response']['result'],
+                         self.NO_SUM_EXPECTED['response']['result'])
+    
+    def test_results_w_sum(self):
+        with NamedTemporaryFile(
+            dir='query_files', suffix='.sql', mode='w',
+            delete=False) as _sql_sum_file:
+            sql_sum_file = _sql_sum_file
+            sql_sum_file.write(TEST_SUM_SQL)
+            _report_name = os.path.basename(sql_sum_file.name)
+            sum_query_name = os.path.splitext(_report_name)[0]
+
+        loader = Query(sum_query_name)
+        result = loader.result_set()
+        formatter = ResultFormat(result, format_='sum')
+        formatted_results = formatter.formatted_results()
+        self.assertEqual(formatted_results['response']['summary'],
+                         self.SUM_EXPECTED['response']['summary'])
+        self.assertEqual(formatted_results['response']['result'],
+                         self.SUM_EXPECTED['response']['result'])
+        os.remove(sql_sum_file.name)
 
 
 class TestAPI(AioHTTPTestCase):
