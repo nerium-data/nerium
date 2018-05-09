35c4e251e82b83816d472ff90c7e4ef570293868
Henry Rizzi <henry.rizzi@adops.com>
2018-05-09 13:53:46 -0500

    update tests for contrib

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import unittest
@@ -63,6 +64,85 @@ class TestResults(unittest.TestCase):
         formatter = ResultFormat(result, format_='default')
         formatted_results = formatter.formatted_results()
         self.assertEqual(formatted_results, EXPECTED)
+
+class TestContribFormatters(unittest.TestCase):
+    AFFIX_EXPECTED = { 'error': False,
+                       'response': [{
+                                    'foo': 1.25,
+                                    'bar': '2017-09-09',
+                                    'quux': 'Hello',
+                                    'quuux': 'Björk Guðmundsdóttir'
+                                }, {
+                                    'foo': 42,
+                                    'bar': '2031-05-25',
+                                    'quux': 'yo',
+                                    'quuux': 'ƺƺƺƺ'
+                                }],
+                        'metadata': {'executed': 'N/A',  
+                                     'params': {}}
+                      }
+    
+    COMPACT_EXPECTED = {
+                        'columns': ['foo', 'bar', 'quux', 'quuux'],
+                        'data': [
+                             (1.25, '2017-09-09', 'Hello', 'Björk Guðmundsdóttir'),
+                             (42, '2031-05-25', 'yo', 'ƺƺƺƺ')]
+    }
+    CSV_EXPECTED = 'foo,bar,quux,quuux\r\n1.25,2017-09-09,Hello,Björk Guðmundsdóttir\r\n42,2031-05-25,yo,ƺƺƺƺ\r\n'
+
+    SUM_EXPECTED = { 'error': False,
+                     'response': {'summary': {},
+                                  'result':[{
+                                             'foo': 1.25,
+                                             'bar': '2017-09-09',
+                                             'quux': 'Hello',
+                                             'quuux': 'Björk Guðmundsdóttir'
+                                         }, {
+                                             'foo': 42,
+                                             'bar': '2031-05-25',
+                                             'quux': 'yo',
+                                             'quuux': 'ƺƺƺƺ'
+                                         }]
+                                },
+                     'metadata': {'executed': 'N/A',  
+                                  'params': {}}
+                      }
+
+    def test_results_affix(self):
+        loader = Query(query_name)
+        result = loader.result_set()
+        formatter = ResultFormat(result, format_='affix')
+        formatted_results = formatter.formatted_results()
+        #match shape
+        self.assertEqual(formatted_results.keys(), self.AFFIX_EXPECTED.keys())
+        self.assertEqual(formatted_results['response'],
+                         self.AFFIX_EXPECTED['response'])
+        self.assertEqual(formatted_results['error'],
+                         self.AFFIX_EXPECTED['error'])
+        self.assertEqual(formatted_results['metadata']['params'],
+                         self.AFFIX_EXPECTED['metadata']['params'])
+
+    def test_results_compact(self):
+        loader = Query(query_name)
+        result = loader.result_set()
+        formatter = ResultFormat(result, format_='compact')
+        formatted_results = formatter.formatted_results()
+        self.assertEqual(formatted_results, self.COMPACT_EXPECTED)
+    
+    def test_results_csv(self):
+        loader = Query(query_name)
+        result = loader.result_set()
+        formatter = ResultFormat(result, format_='csv')
+        formatted_results = formatter.formatted_results()
+        self.assertEqual(formatted_results, self.CSV_EXPECTED) 
+    
+    def test_results_sum(self):
+        loader = Query(query_name)
+        result = loader.result_set()
+        formatter = ResultFormat(result, format_='sum')
+        formatted_results = formatter.formatted_results()
+        self.assertEqual(formatted_results['response']['summary'],
+                         self.SUM_EXPECTED['response']['summary'])  
 
 
 class TestAPI(AioHTTPTestCase):
