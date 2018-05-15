0dbe17ee70bcf3e0fd11029419f83ba3ab9edc98
Henry Rizzi <henry.rizzi@adops.com>
2018-05-15 12:03:02 -0500

    add details to readme+more safety in jinja result set, cleaner dependency

--- nerium/contrib/resultset/jinja.py
+++ nerium/contrib/resultset/jinja.py
@@ -2,30 +2,32 @@ import os
 
 import records
 from dotenv import load_dotenv
-from nerium import ResultSet
+from nerium.contrib.resultset.sql import SQLResultSet
+
 try:
     from jinjasql import JinjaSql
 except ImportError:
-    # jinjasql > 0.1.6 required for use with repeat param def in template
+    # jinjasql >= 0.1.7 required for use with repeat param def in template
     pass
 
 
-class JinjaSQLResultSet(ResultSet):
-    """ SQL database query implementation of ResultSet, using Records library
-    to fetch a dataset from configured query_name
-    """
+class JinjaSQLResultSet(SQLResultSet):
+    """ Use jinjasql to process template for use against records db connection.
+    Similar to a regular SQLResultSet, but preprocesses the query with jinjasql
+    to allow for more complex conditional logic.
 
-    def backend_lookup(self, backend_path):
-        env_location = backend_path / '.env'
-        load_dotenv(env_location, override=True)
-        return os.getenv('DATABASE_URL', 'NOT_CONFIGURED')
+    NOTE: jinjasql>=0.1.7 required for module usage.
+    """
 
     def result(self):
         try:
             backend_path = self.query_path.parent
             backend = self.backend_lookup(backend_path)
             db = records.Database(backend)
-            jinja = JinjaSql(param_style='named')
+            try:
+                jinja = JinjaSql(param_style='named')
+            except NameError:
+                raise Exception("jinjasql >= 0.1.7 required for use")
             with open(self.query_path, 'r') as query_file:
                 query = query_file.readlines()
                 query_text = ''.join(query)
