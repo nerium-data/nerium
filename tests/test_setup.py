5eaa9bf0ddbf89199476ccc2358aea71dd18433e
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-05-29 14:42:52 -0500

    Add config file for tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -2,6 +2,7 @@ import os
 
 os.environ['DATABASE_URL'] = 'sqlite:///'
 os.environ['QUERY_PATH'] = 'tests/query/'
+os.environ['CONFIG_PATH'] = 'tests/test-config.yaml'
 
 query_name = 'basic'
 sum_query_name = 'sum'
