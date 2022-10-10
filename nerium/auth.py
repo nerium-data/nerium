1e660945a872a94d7aedc12f3e168bb53b2f2824
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2022-10-09 19:11:56 -0500

    Rearrange API_KEY presence test logic

--- nerium/auth.py
+++ nerium/auth.py
@@ -14,19 +14,19 @@ def require_api_key(func):
     @functools.wraps(func)
     def decorator(*args, **kwargs):
         API_KEY = os.getenv("API_KEY")
-        # If API_KEY is set in environment, require valid key sent in header
-        if API_KEY:
-            api_key = request.headers.get("X-API-Key")
-            # 400 error if required header is not set
-            if not api_key:
-                return {"error": "Missing API key"}, 400
-            # Check if API key is correct and valid, 403 if not
-            elif api_key == API_KEY:
-                return func(*args, **kwargs)
-            else:
-                return {"error": "The provided API key is not valid"}, 403
-        # If no API_KEY, just pass through
-        else:
+        # If API_KEY is not provided in environment, just pass through
+        if not API_KEY:
             return func(*args, **kwargs)
+
+        # Otherwise API_KEY is set. Require valid key sent in header:
+        api_key = request.headers.get("X-API-Key")
+        # 400 error if required header is not set
+        if not api_key:
+            return {"error": "Missing API key"}, 400
+        # Check if API key is correct and valid, 403 if not
+        elif api_key == API_KEY:
+            return func(*args, **kwargs)
+        else:
+            return {"error": "The provided API key is not valid"}, 403
 
     return decorator
